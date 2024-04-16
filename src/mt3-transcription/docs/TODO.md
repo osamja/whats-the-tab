9310289e6b0f592784c74803ac16d2eb400d4946
Sammy Javed <me@sammyjaved.com>
2024-04-15 22:10:47 -0700

    stable but extending model to support transcription progres

--- src/mt3-transcription/docs/TODO.md
+++ src/mt3-transcription/docs/TODO.md
@@ -1,4 +1,16 @@
 TODO
+	make the apis
+
+		add a status field to model
+
+		add a status api
+
+		install dramatiq and redis message broker
+
+
+
+
+	
 
 IN PROGRESS
 	
@@ -9,4 +21,11 @@ DONE
 	serve the midi file
 
 	push changes to main
+
+	connect server to tailscale
+
+	connect desktop to tailscale
+
+	expose api publicly
+
 
--- src/mt3-transcription/musictranscription/transcribeapp/models.py
+++ src/mt3-transcription/musictranscription/transcribeapp/models.py
@@ -6,3 +6,7 @@ class AudioMIDI(models.Model):
     midi_file = models.FileField(upload_to='midis/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, default='pending')  # Status of the transcription
+    current_segment = models.IntegerField(default=0)  # Current segment being transcribed
+    num_transcription_segments = models.IntegerField(default=1)  # Number
+
