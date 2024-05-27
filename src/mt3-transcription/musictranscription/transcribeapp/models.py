4f249225999a3097252c7591c6b88d777794bc59
sammy <me@sammyjaved.com>
2024-05-27 10:40:39 -0700

    fixed midi stitching bug

--- src/mt3-transcription/musictranscription/transcribeapp/models.py
+++ src/mt3-transcription/musictranscription/transcribeapp/models.py
@@ -3,15 +3,13 @@ from django.db import models
 class AudioMIDI(models.Model):
     audio_filename = models.CharField(max_length=255)
     audio_file = models.FileField(upload_to='audios/')
-    midi_file = models.FileField(upload_to='midis/', blank=True, null=True)
     youtube_url = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=20, default='pending')  # Status of the transcription
-    current_segment = models.IntegerField(default=0)  # Current segment being transcribed
-    num_transcription_segments = models.IntegerField(default=10)  # Number
-    is_midi2wav = models.BooleanField(default=False)  # Whether the audio is a MIDI wav file
-    midi_wav_file = models.FileField(upload_to='midi_2_wav/', blank=True, null=True)  # MIDI as wav file
+    status = models.CharField(max_length=20, default='pending')
+    current_segment = models.IntegerField(default=0)
+    num_transcription_segments = models.IntegerField(default=1)
+    audio_chunk_length = models.IntegerField(default=10)
 
 class AudioChunk(models.Model):
     audio_midi = models.ForeignKey(AudioMIDI, on_delete=models.CASCADE, related_name='audio_chunks')
--- src/mt3-transcription/musictranscription/transcribeapp/tasks.py
+++ src/mt3-transcription/musictranscription/transcribeapp/tasks.py
@@ -12,8 +12,7 @@ from pytube import YouTube
 import tempfile
 import os
 
-
-from .ml import split_audio_segments, transcribe_and_download, copy_acoustic_guitar_events, plot_note_on_times, InferenceModel, stitch_midi_files, midi_files_to_wav, combine_wavs
+from .ml import split_audio_segments, transcribe_and_download, InferenceModel
 
 MODEL = "mt3" #@param["ismir2021", "mt3"]
 mt3_path = 'checkpoints'
@@ -44,17 +43,10 @@ def download_youtube_audio_and_save(audio_midi_id):
 @dramatiq.actor(max_retries=3, min_backoff=1000, max_backoff=10000)
 def generate_midi_from_audio(audio_midi_id):
   audio_midi = AudioMIDI.objects.get(id=audio_midi_id)
-  num_transcription_segments = audio_midi.num_transcription_segments
-  is_midi2wav = audio_midi.is_midi2wav
-
   inference_model = InferenceModel(checkpoint_path, MODEL)
 
-  # mp3 is split into N segments of audio chunk length.
-  # To transcribe entire mp3, num_transcription_segments = len(audio) / audio_chunk_length
-  # To transcribe the first 2 seconds of an mp3, set NUM_TRANSCRIPTION_SEGMENTS to 1 assuming length is 2 seconds
-  NUM_TRANSCRIPTION_SEGMENTS = int(num_transcription_segments)
-  AUDIO_CHUNK_LENGTH = 5000
-  # check if audio is an mp3 or wav file
+  NUM_TRANSCRIPTION_SEGMENTS = int(audio_midi.num_transcription_segments)
+  AUDIO_CHUNK_LENGTH = int(audio_midi.audio_chunk_length) * 1000 # in milliseconds
 
   split_audio_segments(audio_midi, AUDIO_CHUNK_LENGTH, NUM_TRANSCRIPTION_SEGMENTS)
 
@@ -62,46 +54,8 @@ def generate_midi_from_audio(audio_midi_id):
   
   transcribe_and_download(audio_midi, split_filenames, inference_model)
 
-  midi_files = getSplitMIDIFiles(audio_midi)
-
-  if is_midi2wav:
-    wav_files = midi_files_to_wav(midi_files, 'output.wav')
-    # Combine all WAV files into one
-    combined_output = combine_wavs(wav_files)
-    unique_filename = f"combined_output_{now().strftime('%Y%m%d%H%M%S')}.wav"
-    # Save combined WAV file using Django's FileField
-    audio_midi.midi_wav_file.save(unique_filename, ContentFile(combined_output))
-    print("Conversion and combination complete. Output saved as 'combined_output.wav'.")
-
-  # Copy acoustic guitar events to a new MIDI file
-  output_file = 'acoustic_guitar_only.midi'
-  acoustic_guitar_midi = copy_acoustic_guitar_events(midi_files, output_file)
-  midi_file, midi_filename = acoustic_guitar_midi, output_file
-
-  print(f"Acoustic guitar events copied to '{output_file}'")
-
-  # Assuming `output_midi` is your MidiFile object
-  # Save the MidiFile data to a BytesIO object
-  midi_buffer = io.BytesIO()
-  midi_file.save(file=midi_buffer)
-
-  # It's important to seek back to the beginning of the BytesIO object after writing to it
-  midi_buffer.seek(0)
-
-  # Create a Django File object wrapping the BytesIO buffer
-  wrapped_file = File(midi_buffer, name=midi_filename)
-  audio_midi.midi_file = wrapped_file
-  
-  audio_midi.save()
-
   audio_midi.status = 'completed'
   audio_midi.save()
-
-def getSplitMIDIFiles(audio_midi):
-  split_midi_files = []
-  for midi_chunk in audio_midi.midi_chunks.all():
-    split_midi_files.append(midi_chunk.midi_file.path)
-  return split_midi_files
 
 def getSplitFilenames(audio_midi):
   split_filenames = []
