d30f11a0de6cc39f0839b2c8e0c49ded85b86250
Sammy Javed <sammyjaved@gmail.com>
2025-03-26 20:44:26 +0000

    need to create frontend url to collect new password info andtrigger a post request

--- src/mt3-transcription/musictranscription/musictranscription/urls.py
+++ src/mt3-transcription/musictranscription/musictranscription/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from dj_rest_auth.registration.views import ConfirmEmailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,13 +26,19 @@ urlpatterns = [
     path('api/auth/', include('dj_rest_auth.urls')),  # Login, logout, password reset
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),  # Signup API
     path('transcribe/', include('transcribeapp.urls')),
+
+    path(
+        "api/auth/registration/account-confirm-email/<key>/",
+        ConfirmEmailView.as_view(),
+        name='account_confirm_email'
+    ),
     
     # # Password reset URLs
-    # path('api/auth/password/reset/confirm/<uidb64>/<token>/',
-    #      auth_views.PasswordResetConfirmView.as_view(),
-    #      name='password_reset_confirm'),
-    # path('api/auth/password/reset/complete/',
-    #      auth_views.PasswordResetCompleteView.as_view(),
-    #      name='password_reset_complete'),
+    path('api/auth/password/reset/confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('api/auth/password/reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
 
